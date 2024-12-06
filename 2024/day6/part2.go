1e7afa00c6cffddfb28db4b3ee4a48b22907ee90
Vinicius Fabri <v.fabri@youweagency.com>
2024-12-06 11:49:59 +0100

    CPU go brrr

--- 2024/day6/part2.go
+++ 2024/day6/part2.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 
 	day6 "github.com/viniciusfabri/aoc-2024/2024/day6/shared"
 )
@@ -31,6 +32,65 @@ func Part2(input string) string {
 		if guardGetsStuckInLoop(simulatedGrid) {
 			loopCount++
 		}
+	}
+
+	// Alternatively you can parallelize the simulation...
+	// loopCount := countLoopsConcurrently(roomGrid, validObstructionPositions, 8)
+
+	return fmt.Sprint(loopCount)
+}
+
+/**
+ * Chat GPT generated code.
+ */
+func countLoopsConcurrently(roomGrid [][]rune, validObstructionPositions []struct{ row, col int }, numWorkers int) string {
+	type Position struct {
+		row, col int
+	}
+
+	// Worker function to process obstructions
+	worker := func(obstructionCh <-chan Position, resultCh chan<- int, wg *sync.WaitGroup) {
+		defer wg.Done()
+		for obstruction := range obstructionCh {
+			simulatedGrid := day6.CopyGrid(roomGrid)
+			simulatedGrid[obstruction.row][obstruction.col] = '#'
+			if guardGetsStuckInLoop(simulatedGrid) {
+				resultCh <- 1
+			} else {
+				resultCh <- 0
+			}
+		}
+	}
+
+	obstructionCh := make(chan Position, len(validObstructionPositions))
+	resultCh := make(chan int, len(validObstructionPositions))
+
+	var wg sync.WaitGroup
+
+	// Start worker goroutines
+	for i := 0; i < numWorkers; i++ {
+		wg.Add(1)
+		go worker(obstructionCh, resultCh, &wg)
+	}
+
+	// Send all obstruction positions to the channel
+	go func() {
+		for _, obstruction := range validObstructionPositions {
+			obstructionCh <- obstruction
+		}
+		close(obstructionCh)
+	}()
+
+	// Wait for all workers to finish
+	go func() {
+		wg.Wait()
+		close(resultCh)
+	}()
+
+	// Collect results
+	loopCount := 0
+	for result := range resultCh {
+		loopCount += result
 	}
 
 	return fmt.Sprint(loopCount)
